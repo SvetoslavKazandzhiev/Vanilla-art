cd33926b22c5601afa49cdd9a975e7ad32708ef3
SvetoslavKazandzhiev <80912240+SvetoslavKazandzhiev@users.noreply.github.com>
2021-08-20 18:00:19 +0100

    Delete artportal/tests/art_app directory

--- artportal/tests/art_app/forms/art_create_form_test.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase, Client
-from django.urls import reverse
-
-
-class CreateFormTest(TestCase):
-    def test_createform_if_is_valid(self):
-        client = Client()
-        response = self.client.get(reverse('create'))
-        self.assertEqual(response.status_code, 302)
